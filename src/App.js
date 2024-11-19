a5dffbfbd564e5212accf51ade6b94b14eb2a529
MIdasCO <rusabi54@gmail.com>
2024-11-19 10:51:27 +0600

    11/19/2024

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
+import { useState,useEffect } from "react";
 import { ProductCard } from "./components/ProductCard";
 
-const products = [
-  {
-    id:0,
-    name:"Product 1",
-    price: 100,
-    category:"Category 1",
-    imge:"https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress&fm=pjpg"
-  },
-  {
-    id:1,
-    name:"Product 1",
-    price: 100,
-    category:"Category 1",
-    imge:"https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress&fm=pjpg"
-  }
-]
+// const products = [
+//   {
+//     id: 0,
+//     name: "Product 1",
+//     price: 100,
+//     category: "Category 1",
+//     img: "https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress&fm=pjpg"
+//   },
+//   {
+//     id: 1,
+//     name: "Product 2",
+//     price: 200,
+//     category: "Category 2",
+//     img: "https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress&fm=pjpg"
+//   }
+// ];
 
 function App() {
+  const [product, setProducts] = useState([]);
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+  },[])
   return (
     <div>
-      {
-        products.map((product)=>(
-          <ProductCard />
-        ))
-      }
+      {/* <button onClick={()=>setProducts([]})}>123</button> */}
+      {product.map((product) => (
+        <ProductCard data={product} />
+      ))}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,3 +1,14 @@
-export function ProductCard(){
-    return <div>Product Card</div>
+import "./index.css";
+
+export function ProductCard(props) {
+  const { category, image, title, price } = props.data;
+
+  return (
+    <div className="product-card">
+      <img width="200px" src={image} />
+      <div>{category}</div>
+      <h4>{title}</h4>
+      <div>$ {price}</div>
+    </div>
+  );
 }
