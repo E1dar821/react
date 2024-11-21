726445a8159bb5f9461743f7e9b2f7187c202658
MIdasCO <rusabi54@gmail.com>
2024-11-21 23:56:41 +0600

    final

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -5,11 +5,11 @@ export function ProductCard(props) {
 
   return (
     <div className="product-card">
-      <img width="200px" src={image} />
-      <div>{category}</div>
-      <h4>{title}</h4>
-      <div>$ {price}</div>
-      <button>Add to cart</button>
+      <img width="200px" height="200px" src={image} />
+      <div className="category">{category}</div>
+      <div className="title"><h4>{title}</h4></div>
+      <div className="price"> <b>$ {price}</b></div>
+      <button>Add to Card</button>
     </div>
   );
 }
